2cebd9fe0bbd6e8a4abc100c094f2c92217db2f2
kooyeongmin <rn9dud0@naver.com>
2023-12-30 15:46:08 +0900

    K번째수

---
+++ Programmers/K번째수.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> s; // 정렬을 위한 벡터
+    for(int i=0;i<commands.size();i++){
+         int start = commands[i][0];
+         int end = commands[i][1];
+         int target = commands[i][2];
+            
+        s.push_back(array[start-1]);
+        
+        while(start!=end){
+            start++;
+            s.push_back(array[start-1]);
+            
+        }
+            
+        sort(s.begin(), s.end());
+        
+        answer.push_back(s[target-1]);
+        s.clear();
+    }
+    
+    
+    
+    
+    
+    return answer;
+}
