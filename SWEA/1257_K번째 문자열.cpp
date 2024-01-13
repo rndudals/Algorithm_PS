efd932279bfb064e0c705bd42ae8794f6e79e3ad
kooyeongmin <rn9dud0@naver.com>
2024-01-13 11:06:58 +0900

    1257_K번째 문자열

---
+++ SWEA/1257_K번째 문자열.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include <set>
+using namespace std;
+ 
+set<string>s;
+set<string>::iterator it;
+int main(int argc, char** argv)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int test_case;
+    int T;
+    //freopen("input.txt", "r", stdin);
+    cin >> T;
+ 
+    for (test_case = 1; test_case <= T; ++test_case)
+    {
+        int K = 0;
+        string st, tmp = "";
+        cin >> K;
+        cin >> st;
+        int size = st.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                tmp += st[j];
+                s.insert(tmp);
+            }
+            tmp = "";
+        }
+        if (K > s.size()) {
+            cout << "#" << test_case << " " << "none" << '\n';
+ 
+        }
+        else {
+            it = s.begin();
+            for (int i = 0; i < K - 1; i++) { it++; }
+ 
+            cout << "#" << test_case << " " << *it << '\n';
+        }
+         
+ 
+        s.clear();
+ 
+    }
+    return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
