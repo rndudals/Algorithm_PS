7c85188a8b5707b2820681b27bfd1fa01b09c9f6
kooyeongmin <rn9dud0@naver.com>
2024-02-17 12:56:48 +0900

    10815_숫자 카드

---
+++ Baekjoon/10815_숫자카드 v2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+#define size 10000000
+
+int N, M, x;
+bool visited[20000001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		visited[x+size] = true;
+	}
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> x;
+		if (visited[x+size]) {
+			cout << "1"<<" ";
+		}else {
+			cout << "0"<<" ";
+		}
+	}
+	
+	return 0;
+}
