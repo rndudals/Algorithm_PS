b13c26041f49840b19c29a4d0f6a07c9f1e4114d
kooyeongmin <rn9dud0@naver.com>
2024-04-13 17:15:38 +0900

    10807_개수 세기

---
+++ Baekjoon/10807_개수 세기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int ans = 0;
+		
+		int N = scanner.nextInt();
+		
+		int[] arr = new int[N];
+		
+		for(int i=0;i<N;i++) {
+			arr[i] = scanner.nextInt();
+		}
+		
+		int target = scanner.nextInt();
+		
+		for(int i=0;i<N;i++) {
+			if(arr[i] == target) {
+				ans++;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+
+}
