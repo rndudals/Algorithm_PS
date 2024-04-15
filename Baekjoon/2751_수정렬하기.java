22bcb529e88e141b25f69fc6ab922d644004df35
kooyeongmin <rn9dud0@naver.com>
2024-04-15 21:58:02 +0900

    2751_수 정렬하기

---
+++ Baekjoon/2751_수정렬하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		ArrayList <Integer> list = new ArrayList<>();
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			list.add(sc.nextInt());
+		}
+		
+		Collections.sort(list);
+		
+		for(int ans : list) {
+			sb.append(ans).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
