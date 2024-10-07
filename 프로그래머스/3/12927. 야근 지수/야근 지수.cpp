6d80cc831d3ffc57f896a99354bad602d981853f
구영민 <102203336+rndudals@users.noreply.github.com>
2024-10-07 09:55:29 +0900

    [level 3] Title: 야근 지수, Time: 1.59 ms, Memory: 3.76 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int cnt[50001];
+
+
+long long solution(int n, vector<int> works) {
+    long long answer = 0;
+    long long sum = 0;
+    int c = n;
+    for(int i=0;i<works.size();i++){
+        cnt[works[i]]++;
+        sum += works[i];
+    }
+    
+    for(int i=50000;i>0;i--){
+        if(cnt[i]!=0){
+            c--;
+            cnt[i]--;
+            cnt[i-1]++;
+            i++;
+            if(c==0) {break;}
+        }
+    }
+    
+    if(sum <= n){
+        answer = 0;
+    }else{
+        for(int i=50000;i>0;i--){
+            if(cnt[i]!=0){
+                answer += i*i;
+                cnt[i]--;
+                i++;
+            }
+        }
+    }
+    return answer;
+}
