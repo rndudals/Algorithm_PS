65368e64738d0b04fb84f87785732f40f6d0b4ab
구영민 <102203336+rndudals@users.noreply.github.com>
2024-10-08 21:43:22 +0900

    [level 3] Title: 숫자 게임, Time: 12.61 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+    
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    
+    
+    int idx = 0;
+    for(int i=0;i<B.size();i++){
+        if(B[i]>A[idx]){
+            idx++;
+            answer++;
+        }
+    }
+    
+    return answer;
+}
