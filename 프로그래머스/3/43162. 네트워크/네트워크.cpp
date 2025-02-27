0ad7fb0a399e42d047449b69cc75c204b967aea7
구영민 <102203336+rndudals@users.noreply.github.com>
2025-02-27 11:23:57 +0900

    [level 3] Title: 네트워크, Time: 0.06 ms, Memory: 4.25 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,15 +1,17 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
+
 vector<vector<int>> v;
 
-bool visited[200];
+bool visited[201];
 
-void dfs(int x){
-    visited[x]=true;
-    for(int i=0;i<v[x].size();i++){
-        int next= v[x][i];
+void dfs(int c){
+    visited[c] = true;
+    
+    for(int i=0;i<v[c].size();i++){
+        int next = v[c][i];
         if(!visited[next]){
             dfs(next);
         }
@@ -18,15 +20,17 @@ void dfs(int x){
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    v.resize(200);
-    for(int i=0;i < n ; i++){
-        for(int j=i+1;j<n;j++){
+    v.resize(201);
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
             if(computers[i][j]==1){
                 v[i].push_back(j);
-                v[j].push_back(i);    
+                v[j].push_back(i);
             }
         }
     }
+    
     for(int i=0;i<n;i++){
         if(!visited[i]){
             dfs(i);
