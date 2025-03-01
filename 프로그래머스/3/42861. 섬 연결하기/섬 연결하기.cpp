e0f9f90d03f5a41be2f6974624ab11b04bdd751e
구영민 <102203336+rndudals@users.noreply.github.com>
2025-03-01 11:26:13 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -4,13 +4,24 @@
 #include <iostream>
 using namespace std;
 
-vector<pair<int , pair<int, int>>> v;
-int parent[101];
+class A{
+  public:
+    int cost;
+    int x;
+    int y;
+};
+
+int parent[100];
+vector<A> v;
 
 void init(int n){
-    for(int i=0;i<n;i++){
+    for(int i=1;i<=n;i++){
         parent[i] = i;
     }
+}
+
+bool compare(A a, A b){
+    return a.cost<b.cost;
 }
 
 int getParent(int x){
@@ -20,31 +31,31 @@ int getParent(int x){
     return parent[x] = getParent(parent[x]);
 }
 
-void unionNode(int a, int b){
-    if(a>b){swap(a,b);}
-    parent[getParent(b)] = getParent(a);
+void unionNode(int x, int y){
+    if(y<x) swap(x, y);
+    parent[getParent(y)] = getParent(x);
 }
-
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
-    
     init(n);
-    
     for(int i=0;i<costs.size();i++){
-        v.push_back({costs[i][2], {costs[i][0], costs[i][1]}});
+        v.push_back({costs[i][2], costs[i][0], costs[i][1]});
     }
+    sort(v.begin(), v.end(), compare);
     
-    sort(v.begin(), v.end());
     
-    
+    int cnt = 0;
     for(int i=0;i<v.size();i++){
-        int cost = v[i].first;
-        int start = v[i].second.first;
-        int end = v[i].second.second;
+        int x = v[i].x;
+        int y = v[i].y;
+        int cost = v[i].cost;
         
-        if(getParent(start)!=getParent(end)){
-            unionNode(start, end);
+        if(getParent(x)!=getParent(y)){
+            unionNode(x, y);
+            cnt++;
             answer+=cost;
+
+            if(cnt == n) break;
         }
     }
     return answer;
