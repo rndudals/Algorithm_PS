7410ebe0fd414b0d4c3c6f187cd2ab4017dc2f4c
구영민 <102203336+rndudals@users.noreply.github.com>
2025-03-10 21:29:26 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<sstream>
+#include<algorithm>
+#include<queue>
+#include<string>
+using namespace std;
+
+
+int N;
+
+
+int main(int argc, char** argv)
+{
+	cin>>N;
+	if(N%2==1){
+		cout<<"SK";
+	}else{
+		cout<<"CY";
+	}
+}
