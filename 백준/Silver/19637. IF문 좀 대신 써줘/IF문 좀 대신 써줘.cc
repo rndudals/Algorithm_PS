c45a7adff8471868b92a4db41e55a1fa4f49be69
구영민 <102203336+rndudals@users.noreply.github.com>
2025-04-09 22:15:29 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 48 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <map>
+#include <stack>
+#include <set>
+#include <math.h>
+#include <unordered_map>
+
+using namespace std;
+
+int N, M;
+vector<pair<int, string>> v;
+
+string bs(int n){
+    int s = 0;
+    int e = v.size();
+
+    while(s<e){
+        int mid = (e-s)/2 + s;
+        if(v[mid].first < n){
+            s = mid+1;
+        }else{
+            e = mid;
+        }
+    }
+    return v[e].second;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin>>N>>M;
+
+    int start = 0;
+    for(int i=0;i<N;i++){
+        string st;
+        int x;
+        cin >> st >> x;
+        v.push_back({x, st});
+    }
+    for(int i=0;i<M;i++){
+        int n;
+        cin>>n;
+        cout<< bs(n)<<'\n';
+    }
+    return 0;
+}
